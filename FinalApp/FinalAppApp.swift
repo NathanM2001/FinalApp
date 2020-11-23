fb1359c8c739a5e0219cf85a77375699993ed30d
nathanmckenzie <ngm18@acu.edu>
2020-11-23 13:13:17 -0600

    Initial Commit

---
+++ FinalApp/FinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalAppApp.swift
+//  FinalApp
+//
+//  Created by Nathan Mckenzie on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct FinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
